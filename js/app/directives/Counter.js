a3eaebcc00653bd014e11bef86dcbd0c75e3996a
lisamarie616 <lisamarie616@gmail.com>
2016-04-10 18:37:17 -0700

    Done.

--- js/app/directives/Counter.js
+++ js/app/directives/Counter.js
@@ -1,14 +1,27 @@
 function Counter() {
 	return {
+		transclude: true,
 		template: [
 			'<div>',
 				'<h3>Counter</h3>',
 				'<div>Click anywhere to increment the counter!</div>',
-				'<div>Current count: {{ count }}</div>',
+				'<div>Current count: {{ ctrl.count }}</div>',
 			'</div>'
 		].join(''),
-		controller: function ($scope) {
-			$scope.count = 0;
+		require: 'counter',
+		controller: function () {
+			this.count = 0;
+		},
+		controllerAs: 'ctrl',
+		link: function(scope, element, attr, ctrl){
+			function incrementCount(){
+				ctrl.count++;
+				scope.$apply();
+			}
+			element.on('click', incrementCount);
+			scope.$on('$destroy', function(){
+				element.off();
+			});
 		}
 	}
 }
